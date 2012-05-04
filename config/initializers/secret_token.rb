d1fae2ffbe9f5f9c86fa43527beabd2ec67f446c
Kevin Fahy <kevin.fahy@gmail.com>
2012-05-03 22:51:54 -0700

    Base rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkatingCalc::Application.config.secret_token = 'fbe9f0ff20ef2d775b396d7e1826226583b83fe60dc4ec4cc04bba023137903b56932ba4c5f4f3167b6cda119a945882a323d109feef7dc92030e673203d1f94'
