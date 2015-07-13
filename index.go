32757c961b75729aefed8d0c87fe907a7da6fb21
Kevin Fahy <kevin.fahy@gmail.com>
2015-07-12 22:03:22 -0700

    Use os.Getenv() to get the port

--- index.go
+++ index.go
@@ -3,12 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 
-	log.Println("Listening on port 3000...")
-	http.ListenAndServe(":3000", nil)
+	log.Println("Listening on port " + port + "...")
+	http.ListenAndServe(":"+port, nil)
 }
